6f4b35911a825a364aacb75086acf6b6ea651466
Szymon Olewniczak <solewniczak@rid.pl>
2020-06-26 13:34:31 +0200

    pluginwiz

---
+++ syntax/table.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * DokuWiki Plugin structat (Syntax Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Szymon Olewniczak <it@rid.pl>
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) {
+    die();
+}
+
+class syntax_plugin_structat_table extends DokuWiki_Syntax_Plugin
+{
+    /**
+     * @return string Syntax mode type
+     */
+    public function getType()
+    {
+        return 'FIXME: container|baseonly|formatting|substition|protected|disabled|paragraphs';
+    }
+
+    /**
+     * @return string Paragraph type
+     */
+    public function getPType()
+    {
+        return 'FIXME: normal|block|stack';
+    }
+
+    /**
+     * @return int Sort order - Low numbers go before high numbers
+     */
+    public function getSort()
+    {
+        return FIXME;
+    }
+
+    /**
+     * Connect lookup pattern to lexer.
+     *
+     * @param string $mode Parser mode
+     */
+    public function connectTo($mode)
+    {
+        $this->Lexer->addSpecialPattern('<FIXME>', $mode, 'plugin_structat_table');
+//        $this->Lexer->addEntryPattern('<FIXME>', $mode, 'plugin_structat_table');
+    }
+
+//    public function postConnect()
+//    {
+//        $this->Lexer->addExitPattern('</FIXME>', 'plugin_structat_table');
+//    }
+
+    /**
+     * Handle matches of the structat syntax
+     *
+     * @param string       $match   The match of the syntax
+     * @param int          $state   The state of the handler
+     * @param int          $pos     The position in the document
+     * @param Doku_Handler $handler The handler
+     *
+     * @return array Data for the renderer
+     */
+    public function handle($match, $state, $pos, Doku_Handler $handler)
+    {
+        $data = array();
+
+        return $data;
+    }
+
+    /**
+     * Render xhtml output or metadata
+     *
+     * @param string        $mode     Renderer mode (supported modes: xhtml)
+     * @param Doku_Renderer $renderer The renderer
+     * @param array         $data     The data from the handler() function
+     *
+     * @return bool If rendering was successful.
+     */
+    public function render($mode, Doku_Renderer $renderer, $data)
+    {
+        if ($mode !== 'xhtml') {
+            return false;
+        }
+
+        return true;
+    }
+}
+
