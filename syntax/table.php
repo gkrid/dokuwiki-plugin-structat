9b32cce9d73b905dbe9cf5377de89272375aaf85
Szymon Olewniczak <solewniczak@rid.pl>
2020-06-27 11:59:31 +0200

    first working version

--- syntax/table.php
+++ syntax/table.php
@@ -7,18 +7,27 @@
  */
 
 // must be run within Dokuwiki
+use dokuwiki\plugin\struct\meta\AggregationTable;
+use dokuwiki\plugin\struct\meta\ConfigParser;
+use dokuwiki\plugin\struct\meta\StructException;
+use dokuwiki\plugin\structat\meta\SearchConfigAt;
+
 if (!defined('DOKU_INC')) {
     die();
 }
 
 class syntax_plugin_structat_table extends DokuWiki_Syntax_Plugin
 {
+
+    /** @var string which class to use for output */
+    protected $tableclass = AggregationTable::class;
+
     /**
      * @return string Syntax mode type
      */
     public function getType()
     {
-        return 'FIXME: container|baseonly|formatting|substition|protected|disabled|paragraphs';
+        return 'substition';
     }
 
     /**
@@ -26,7 +35,7 @@ class syntax_plugin_structat_table extends DokuWiki_Syntax_Plugin
      */
     public function getPType()
     {
-        return 'FIXME: normal|block|stack';
+        return 'block';
     }
 
     /**
@@ -34,7 +43,7 @@ class syntax_plugin_structat_table extends DokuWiki_Syntax_Plugin
      */
     public function getSort()
     {
-        return FIXME;
+        return 155;
     }
 
     /**
@@ -44,48 +53,86 @@ class syntax_plugin_structat_table extends DokuWiki_Syntax_Plugin
      */
     public function connectTo($mode)
     {
-        $this->Lexer->addSpecialPattern('<FIXME>', $mode, 'plugin_structat_table');
-//        $this->Lexer->addEntryPattern('<FIXME>', $mode, 'plugin_structat_table');
+        $this->Lexer->addSpecialPattern('----+ *structat table *-+\n.*?\n----+', $mode, 'plugin_structat_table');
     }
 
-//    public function postConnect()
-//    {
-//        $this->Lexer->addExitPattern('</FIXME>', 'plugin_structat_table');
-//    }
-
     /**
-     * Handle matches of the structat syntax
+     * Handle matches of the struct syntax
      *
-     * @param string       $match   The match of the syntax
-     * @param int          $state   The state of the handler
-     * @param int          $pos     The position in the document
+     * @param string $match The match of the syntax
+     * @param int $state The state of the handler
+     * @param int $pos The position in the document
      * @param Doku_Handler $handler The handler
-     *
      * @return array Data for the renderer
      */
     public function handle($match, $state, $pos, Doku_Handler $handler)
     {
-        $data = array();
+        global $conf;
 
-        return $data;
+        $lines = explode("\n", $match);
+        array_shift($lines);
+        array_pop($lines);
+
+        try {
+            $parser = new ConfigParser($lines);
+            $config = $parser->getConfig();
+
+            $config = $this->addTypeFilter($config);
+
+            return $config;
+        } catch (StructException $e) {
+            msg($e->getMessage(), -1, $e->getLine(), $e->getFile());
+            if ($conf['allowdebug']) msg('<pre>' . hsc($e->getTraceAsString()) . '</pre>', -1);
+            return null;
+        }
     }
 
     /**
      * Render xhtml output or metadata
      *
-     * @param string        $mode     Renderer mode (supported modes: xhtml)
+     * @param string $mode Renderer mode (supported modes: xhtml)
      * @param Doku_Renderer $renderer The renderer
-     * @param array         $data     The data from the handler() function
-     *
+     * @param array $data The data from the handler() function
      * @return bool If rendering was successful.
      */
     public function render($mode, Doku_Renderer $renderer, $data)
     {
-        if ($mode !== 'xhtml') {
-            return false;
+        if (!$data) return false;
+        global $INFO;
+        global $conf;
+
+        try {
+            $search = new SearchConfigAt($data);
+            if ($mode == 'struct_csv') {
+                // no pagination in export
+                $search->setLimit(0);
+                $search->setOffset(0);
+            }
+
+            /** @var AggregationTable $table */
+            $table = new $this->tableclass($INFO['id'], $mode, $renderer, $search);
+            $table->render();
+
+            if ($mode == 'metadata') {
+                /** @var Doku_Renderer_metadata $renderer */
+                $renderer->meta['plugin']['struct']['hasaggregation'] = $search->getCacheFlag();
+            }
+        } catch (StructException $e) {
+            msg($e->getMessage(), -1, $e->getLine(), $e->getFile());
+            if ($conf['allowdebug']) msg('<pre>' . hsc($e->getTraceAsString()) . '</pre>', -1);
         }
 
         return true;
     }
-}
 
+    /**
+     * Filter based on primary key columns, applicable in child classes
+     *
+     * @param array $config
+     * @return array
+     */
+    protected function addTypeFilter($config)
+    {
+        return $config;
+    }
+}
